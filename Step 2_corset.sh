98a4aa96bbb287ec8f3b752755be3932604b9df9
JUSTIN <mayjj0987@gmail.com>
2025-10-14 14:35:28 +0800

    Update Step 2_corset.sh

--- Step 2_corset.sh
+++ Step 2_corset.sh
@@ -7,30 +7,47 @@ S2_BASE="/home/jade/1_sandsmelt/rundrap/out_drap_s2"
 S3_BASE="/home/jade/1_sandsmelt/rundrap/meta_s3"
 SALMON_QUANT_BASE="$S1_BASE/salmon_quant"
 
-TX_INFO="transcripts_fpkm0_s1 $S1_BASE/meta_BR/d-cov_filter/transcripts_fpkm_0.fa
-transcripts_fpkm1_s1 $S1_BASE/meta_BR/d-cov_filter/transcripts_fpkm_1.fa
-transcripts_fpkm0_s2 $S2_BASE/transcripts_fpkm_0.fa
-transcripts_fpkm0.5_s2 $S2_BASE/transcripts_fpkm_0.5.fa
-transcripts_fpkm0_s3 $S3_BASE/transcripts_fpkm_0.fa
-transcripts_fpkm0.5_s3 $S3_BASE/transcripts_fpkm_0.5.fa
-transcripts_fpkm1_s3 $S3_BASE/transcripts_fpkm_1.fa"
+# Explicit sample names (comma‑separated, 28 total)
+NAMES="2R,5R,6R,7R,8R,9R,10R,2B,5B,6B,7B,8B,9B,10B,11R,12R,13R,14R,16R,17R,18R,11B,12B,13B,14B,16B,17B,18B"
 
-SAMPLES="2R 5R 6R 7R 8R 9R 10R 2B 5B 6B 7B 8B 9B 10B 11R 12R 13R 14R 16R 17R 18R 11B 12B 13B 14B 16B 17B 18B"
-NAMES=$(printf "%s," $SAMPLES); NAMES=${NAMES%,}
-
+# Explicit group IDs (28 values: 7×1, 7×2, 7×3, 7×4)
 GROUPS="1,1,1,1,1,1,1,2,2,2,2,2,2,2,3,3,3,3,3,3,3,4,4,4,4,4,4,4"
 
-printf "%s\n" "$TX_INFO" | while read NAME FASTA; do
-  [ -z "$NAME" ] && continue
-  OUTPREFIX="corset_${NAME}"
-  FILES=""
-  for S in $SAMPLES; do
-    FILES="$FILES $SALMON_QUANT_BASE/$NAME/$S/aux_info/eq_classes.txt"
-  done
-  echo "[RUN] Corset for $NAME"
-  corset -i salmon_eq_classes -p "$OUTPREFIX" -n "$NAMES" -g 1,1,1,1,1,1,1,2,2,2,2,2,2,2,3,3,3,3,3,3,3,4,4,4,4,4,4,4 $FILES &
-done
+echo "[RUN] Corset for transcripts_fpkm0_s1"
+corset -i salmon_eq_classes -p corset_transcripts_fpkm0_s1 \
+  -n "$NAMES" -g "$GROUPS" \
+  $SALMON_QUANT_BASE/transcripts_fpkm0_s1/*/aux_info/eq_classes.txt &
 
-# Wait for all backgrounded corset jobs to finish
+echo "[RUN] Corset for transcripts_fpkm1_s1"
+corset -i salmon_eq_classes -p corset_transcripts_fpkm1_s1 \
+  -n "$NAMES" -g "$GROUPS" \
+  $SALMON_QUANT_BASE/transcripts_fpkm1_s1/*/aux_info/eq_classes.txt &
+
+echo "[RUN] Corset for transcripts_fpkm0_s2"
+corset -i salmon_eq_classes -p corset_transcripts_fpkm0_s2 \
+  -n "$NAMES" -g "$GROUPS" \
+  $SALMON_QUANT_BASE/transcripts_fpkm0_s2/*/aux_info/eq_classes.txt &
+
+echo "[RUN] Corset for transcripts_fpkm0.5_s2"
+corset -i salmon_eq_classes -p corset_transcripts_fpkm0.5_s2 \
+  -n "$NAMES" -g "$GROUPS" \
+  $SALMON_QUANT_BASE/transcripts_fpkm0.5_s2/*/aux_info/eq_classes.txt &
+
+echo "[RUN] Corset for transcripts_fpkm0_s3"
+corset -i salmon_eq_classes -p corset_transcripts_fpkm0_s3 \
+  -n "$NAMES" -g "$GROUPS" \
+  $SALMON_QUANT_BASE/transcripts_fpkm0_s3/*/aux_info/eq_classes.txt &
+
+echo "[RUN] Corset for transcripts_fpkm0.5_s3"
+corset -i salmon_eq_classes -p corset_transcripts_fpkm0.5_s3 \
+  -n "$NAMES" -g "$GROUPS" \
+  $SALMON_QUANT_BASE/transcripts_fpkm0.5_s3/*/aux_info/eq_classes.txt &
+
+echo "[RUN] Corset for transcripts_fpkm1_s3"
+corset -i salmon_eq_classes -p corset_transcripts_fpkm1_s3 \
+  -n "$NAMES" -g "$GROUPS" \
+  $SALMON_QUANT_BASE/transcripts_fpkm1_s3/*/aux_info/eq_classes.txt &
+
+# Wait for all backgrounded jobs
 wait
-echo "[DONE] All Corset runs completed in parallel."
+echo "[DONE] All 7 Corset runs completed in parallel."
